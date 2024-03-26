1f14bbc1d1c466835cfc7e53ad94065da85fc12a
Cory House <housecor@gmail.com>
2024-03-26 14:25:00 -0500

    a

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,23 @@ import ReactDOM from "react-dom/client";
 import Products from "./Products.tsx";
 import "./index.css";
 import { ErrorBoundary } from "react-error-boundary";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { About } from "./About.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary fallback={<h1>Oops! An error occurred.</h1>}>
       <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Products</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
         <Routes>
           <Route path="/" Component={Products} />
           <Route path="/about" Component={About} />
