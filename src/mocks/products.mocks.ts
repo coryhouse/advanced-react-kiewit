dccf18fbca0ed4c62d67128a627d7ef15ddb6a9b
Cory House <housecor@gmail.com>
2024-03-28 11:13:31 -0500

    add

---
+++ src/mocks/products.mocks.ts
@@ -0,0 +1,48 @@
+import { HttpResponse, http } from "msw";
+import { Product } from "../types/product";
+
+export const scenarios = {
+  error: [
+    http.get("/user", () => {
+      return new HttpResponse(null, { status: 500 });
+    }),
+  ],
+  success: [
+    http.get(
+      "http://localhost:3001/products",
+      ({ request, params, cookies }) => {
+        const mockProducts: Product[] = [
+          {
+            id: 1,
+            name: "Product 1",
+            description: "Description 1",
+            price: 100,
+            image: "image1.jpg",
+            category: "category1",
+            skus: [],
+          },
+          {
+            id: 2,
+            name: "Product 2",
+            description: "Description 2",
+            price: 200,
+            image: "image2.jpg",
+            category: "category2",
+            skus: [],
+          },
+          {
+            id: 3,
+            name: "Product 3",
+            description: "Description 3",
+            price: 300,
+            image: "image3.jpg",
+            category: "category3",
+            skus: [],
+          },
+        ];
+
+        return HttpResponse.json(mockProducts);
+      }
+    ),
+  ],
+};
